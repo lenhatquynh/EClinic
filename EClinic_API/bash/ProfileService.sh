d0aefb4bddbf93af156aaad7565004d60b2cc940
lenhatquynh <nhatquynh24092001@gmail.com>
2024-05-11 15:59:58 +0700

    update sql server port and docker for front-end

--- EClinic_API/bash/ProfileService.sh
+++ EClinic_API/bash/ProfileService.sh
@@ -1,7 +1,7 @@
 cd ..
 cd Project.ProfileService
-dotnet ef database update --connection "Data Source=localhost,1444;Initial Catalog=ProfileService;Persist Security Info=True;User ID=sa;Password=Eclinic123;"
-sqlcmd -S localhost,1444 -U sa -d ProfileService -i "../Database/ProfileService.sql"
+dotnet ef database update --connection "Data Source=localhost,1433;Initial Catalog=ProfileService;Persist Security Info=True;User ID=sa;Password=Eclinic123;"
+sqlcmd -S localhost,1433 -U sa -d ProfileService -i "../Database/ProfileService.sql"
 cd ..
 cd bash
 
